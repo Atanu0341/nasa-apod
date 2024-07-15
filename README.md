0575c59a94310a2658ae18ee6de3b2b1f80a58d8
Atanu Majumder <atanumajumder2004@gmail.com>
2024-07-15 09:58:48 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+
+  # NASA Astronomy Picture of the Day 🌌
+
+  Discover the cosmos! Each day a different image or photograph of our fascinating universe is featured, along with a brief explanation written by a professional astronomer.
+
+![NASA APOD](https://apod.nasa.gov/apod/image/2407/M33Meteor_Chokshi_2000.jpg)
+
+## Meteor Misses Galaxy
+
+15th July 2024
+
+### Explanation: 
+
+The galaxy was never in danger. For one thing, the Triangulum galaxy (M33), pictured, is much bigger than the tiny grain of rock at the head of the meteor. For another, the galaxy is much farther away -- in this instance 3 million light years as opposed to only about 0.0003 light seconds.  Even so, the meteor's path took it angularly below the galaxy. Also the wind high in Earth's atmosphere blew the meteor's glowing evaporative molecule train away from the galaxy, in angular projection.  Still, the astrophotographer was quite lucky to capture both a meteor and a galaxy in a single exposure -- which was subsequently added to two other images of M33 to bring up the spiral galaxy's colors.  At the end, the meteor was gone in a second, but the galaxy will last billions of years.   Your Sky Surprise: What picture did APOD feature on your birthday? (post 1995)
+
+> _Last Updated: 7/15/2024, 2:50:26 AM (in GMT)_
---
+++ index.ts
@@ -0,0 +1,75 @@
+import { writeFileSync } from "fs";
+
+async function getAPOD() {
+  const apiKey = process.env.API_KEY as string;
+  const url = `https://api.nasa.gov/planetary/apod?api_key=Xi8NOs3v7Ogxe2Edj2U3v0HHkgR9EcfATmIgepkA`;
+
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching APOD: ", error);
+    throw error;
+  }
+}
+
+// Get the current date suppose 2024-04-02 in the format 2nd April, 2024
+
+const currentDate = new Date();
+const day = currentDate.getDate();
+let daySuffix;
+if (day === 1 || day === 21 || day === 31) {
+  daySuffix = "st";
+} else if (day === 2 || day === 22) {
+  daySuffix = "nd";
+} else if (day === 3 || day === 23) {
+  daySuffix = "rd";
+} else {
+  daySuffix = "th";
+}
+
+const formattedDate = `${day}${daySuffix} ${currentDate.toLocaleString('default', { month: 'long' })} ${currentDate.getFullYear()}`;
+
+
+async function updateREADME(apodData: any) {
+  const formatterDate = new Intl.DateTimeFormat("en-US", {
+    dateStyle: "long",
+  });
+  const timestamp = new Date().toLocaleString();
+
+  const content = `
+  # NASA Astronomy Picture of the Day 🌌
+
+  Discover the cosmos! Each day a different image or photograph of our fascinating universe is featured, along with a brief explanation written by a professional astronomer.
+
+![NASA APOD](${apodData.hdurl})
+
+## ${apodData.title}
+
+${formattedDate}
+
+### Explanation: 
+
+${
+  apodData && apodData.explanation
+  ? apodData.explanation.split('NASA Coverage').join('\n### NASA Coverage')
+  : "No explanation available."
+}
+
+> _Last Updated: ${timestamp} (in GMT)_
+`;
+
+  writeFileSync("README.md", content);
+}
+
+async function main() {
+  try {
+    const apodData = await getAPOD();
+    await updateREADME(apodData);
+  } catch (error) {
+    console.error("Error: ", error);
+  }
+}
+
+main();
